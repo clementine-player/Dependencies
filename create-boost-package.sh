a05e09b35cbd82702efa8c4e24678c975efbf37c
John Maguire <john.maguire@gmail.com>
2012-07-12 17:00:47 +0200

    Bump Boost to 1.50 and include random.

--- create-boost-package.sh
+++ create-boost-package.sh
@@ -9,7 +9,7 @@ headers="aligned_storage archive array assert bind call_traits checked_delete
         preprocessor ref scoped_ptr scope_exit serialization shared_ptr smart_ptr
         static_assert swap throw_exception tuple type type_traits utility version
         visit_each enable_shared_from_this scoped_array function bimap concept_check
-        concept foreach blank"
+        concept foreach blank random local_function pending"
 
 if [ -z $path ]; then
   echo "Usage: $0 <path>"
