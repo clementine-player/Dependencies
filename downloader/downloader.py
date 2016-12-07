c49a2d9e11eaa91f2cda529e896f3363f417184e
John Maguire <john.maguire@gmail.com>
2016-12-07 17:31:24 +0000

    Bump gpg-error version to 1.25

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -4,15 +4,16 @@ import os
 import sys
 import urllib
 
-
 DOWNLOAD_URL = 'https://storage.googleapis.com/clementine-data.appspot.com/Build%20dependencies/'
 
 FILES = [
     # filename, md5sum
     ('boost-1.50.tar.bz2', '5c0fdd406d965855e77f435d63a5d729'),
     ('chromaprint-1.1.tar.gz', '6db0b5240231b7d2e6628f49fc765b4a'),
-    ('davidsansome-liblastfm-0.3.1-17-g6748fcf.tar.gz', 'f8e739e24c91f74fca2d6a67106d8f7a'),
-    ('davidsansome-qtsparkle-28721b5.tar.gz', '5d41ff15aaa85f43b9611bbf654b519e'),
+    ('davidsansome-liblastfm-0.3.1-17-g6748fcf.tar.gz',
+     'f8e739e24c91f74fca2d6a67106d8f7a'),
+    ('davidsansome-qtsparkle-28721b5.tar.gz',
+     '5d41ff15aaa85f43b9611bbf654b519e'),
     ('dlfcn-win32-static-r19.tar.bz2', '2c8d5b7767c2e2e4a38bf6a22364cc00'),
     ('faac-1.28.tar.gz', '80763728d392c7d789cde25614c878f6'),
     ('faad2-2.7.tar.gz', 'ee1b4d67ea2d76ee52c5621bc6dbf61e'),
@@ -37,7 +38,7 @@ FILES = [
     ('libechonest-2.3.0.tar.gz', 'c0b39c5029809fbbdd0915ec54e732ba'),
     ('libffi-3.0.11.tar.gz', 'f69b9693227d976835b4857b1ba7d0e3'),
     ('libgcrypt-1.6.1.tar.bz2', 'a5a5060dc2f80bcac700ab0236ea47dc'),
-    ('libgpg-error-1.12.tar.bz2', '8f0eb41a344d19ac2aa9bd101dfb9ce6'),
+    ('libgpg-error-1.25.tar.bz2', 'd9fa545922a5060cbfbd87464bc31686'),
     ('libgpod-0.8.0.3.tar.gz', 'e3633f4ddf1be74cdededb32e2022e2d'),
     ('libiconv-1.14.tar.gz', 'e34509b1623cec449dfeb73d7ce9c6c6'),
     ('libid3tag-0.15.1b.tar.gz', 'e5808ad997ba32c498803822078748c3'),
@@ -53,8 +54,10 @@ FILES = [
     ('libproxy-0.4.11.tar.gz', '3cd1ae2a4abecf44b3f24d6639d2cd84'),
     ('libquicktime-1.1.5.tar.gz', '0fd45b3deff0317c2f85a34b1b106acf'),
     ('libsoup-2.48.0.tar.xz', '51ccb76f5262945e1f0316fdc46de567'),
-    ('libspotify-12.1.45-Darwin-universal.zip', '255ae97cb6a108575c66f6fad37a5990'),
-    ('libspotify-12.1.45-win32-release.zip', '27651f4d0139c8683dd047a25095fc5e'),
+    ('libspotify-12.1.45-Darwin-universal.zip',
+     '255ae97cb6a108575c66f6fad37a5990'),
+    ('libspotify-12.1.45-win32-release.zip',
+     '27651f4d0139c8683dd047a25095fc5e'),
     ('libtasn1-3.4.tar.gz', '21ec021c534b0f30b2834ce233c70f15'),
     ('libtunepimp-0.5.3.tar.gz', '09649f983acef679a548344ba7a9bb2f'),
     ('libusb-1.0.8.tar.bz2', '37d34e6eaa69a4b645a19ff4ca63ceef'),
@@ -68,8 +71,10 @@ FILES = [
     ('protobuf-2.5.0.tar.bz2', 'a72001a9067a4c2c4e0e836d0f92ece4'),
     ('pthreads-win32-2.8.0.tar.gz', '66bba1fc3713f9bf070eca539139def8'),
     ('qjson-0.8.1.tar.gz', '4eef13da988edf8f91c260a3e1baeea9'),
-    ('qt-everywhere-opensource-src-4.8.3.tar.gz', 'a663b6c875f8d7caa8ac9c30e4a4ec3b'),
-    ('qt-everywhere-opensource-src-4.8.6.tar.gz', '2edbe4d6c2eff33ef91732602f3518eb'),
+    ('qt-everywhere-opensource-src-4.8.3.tar.gz',
+     'a663b6c875f8d7caa8ac9c30e4a4ec3b'),
+    ('qt-everywhere-opensource-src-4.8.6.tar.gz',
+     '2edbe4d6c2eff33ef91732602f3518eb'),
     ('sparsehash-2.0.2.tar.gz', '1db92ed7f257d9b5f14a309d75e8a1d4'),
     ('speex-1.2rc1.tar.gz', 'c4438b22c08e5811ff10e2b06ee9b9ae'),
     ('sqlite-amalgamation-3.7.0.tar.gz', '61b85f108760f91b79afc7833e6e6cb4'),
@@ -120,8 +125,8 @@ def DownloadFiles(flags):
     # If the checksum still didn't match the download must have failed.
     if actual_md5_checksum != md5_checksum:
       raise Exception(
-          'Download failed - checksums do not match (got %s, expected %s)' % (
-              actual_md5_checksum, md5_checksum))
+          'Download failed - checksums do not match (got %s, expected %s)' %
+          (actual_md5_checksum, md5_checksum))
 
   print 'All files are up-to-date'
 
