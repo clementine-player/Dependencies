5855ec38f286547ee37028c7249b981b3192dacd
John Maguire <john.maguire@gmail.com>
2016-12-13 19:47:02 +0100

    More stuff cross compiles for mac

--- macosx/Toolchain-Darwin.cmake
+++ macosx/Toolchain-Darwin.cmake
@@ -1,15 +1,14 @@
-include(CMakeForceCompiler)
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_NAME Darwin)
 
 # which compilers to use for C and C++
-cmake_force_c_compiler(x86_64-apple-darwin-clang GNU)
-cmake_force_cxx_compiler(x86_64-apple-darwin-clang++ GNU)
+set(CMAKE_C_COMPILER x86_64-apple-darwin-clang)
+set(CMAKE_CXX_COMPILER x86_64-apple-darwin-clang++)
 
-SET(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.10.sdk)
+set(CMAKE_OSX_SYSROOT /Developer/SDKs/MacOSX10.10.sdk)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_OSX_SYSROOT} ${CMAKE_OSX_SYSROOT}/usr/bin)
+set(CMAKE_FIND_ROOT_PATH /target ${CMAKE_OSX_SYSROOT})
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
